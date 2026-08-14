7fd58774b761170aadd6beedeefcb53567851602
shivam kumar dubey <shivamdubey50773@gmail.com>
2026-08-15 03:11:08 +0530

    Time: 0 ms (100%), Space: 8.8 MB (10.3%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         string word3(word1.size() + word2.size(), ' ');
-        int i,j,k=0;
-        for(i=0,j=0;i<word1.size()&&j<word2.size();i++,j++)
+        int i,k=0;
+        for(i=0;i<word1.size()&&i<word2.size();i++)
         {
             word3[k++]=word1[i];
-            word3[k++]=word2[j];
+            word3[k++]=word2[i];
         }
         while(i<word1.size())
             word3[k++]=word1[i++];
-        while(j<word2.size())
-            word3[k++]=word2[j++];
+        while(i<word2.size())
+            word3[k++]=word2[i++];
         return word3;
     }
 };

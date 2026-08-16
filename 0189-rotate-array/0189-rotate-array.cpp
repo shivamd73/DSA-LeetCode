279c5db98a2522074ae7cc02bb6ee18c9f8288a5
shivam kumar dubey <shivamdubey50773@gmail.com>
2026-08-17 03:14:19 +0530

    Time: 8 ms (4.5%), Space: 264 MB (6.02%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        // int arr[nums.size()],j=0;
-        // k%=nums.size();
-        // for(int i=nums.size()-k;i<nums.size();i++)
-        //     arr[j++]=nums[i];
-        // for(int i=0;i<nums.size()-k;i++)
-        //     arr[j++]=nums[i];
-        // for(int i=0;i<nums.size();i++)
-        //     nums[i]=arr[i];
+        int arr[nums.size()],j=0;
         k%=nums.size();
-        if(k!=0)
-        {
-        int arr[k],j=0;
         for(int i=nums.size()-k;i<nums.size();i++)
             arr[j++]=nums[i];
-        for(int i=nums.size()-k-1;i>=0;i--)
-            nums[i+k]=nums[i];
-        for(int i=0;i<k;i++)
+        for(int i=0;i<nums.size()-k;i++)
+            arr[j++]=nums[i];
+        for(int i=0;i<nums.size();i++)
             nums[i]=arr[i];
-        }
+        // k%=nums.size();
+        // if(k!=0)
+        // {
+        // long int arr[k],j=0;
+        // for(int i=nums.size()-k-1;i>=0;i--)
+        // {
+        //     if(j<k)
+        //         arr[j++]=nums[i+k];
+        //     nums[i+k]=nums[i];
+        // }
+        // j=k-1;
+        // for(int i=0;i<k;i++)
+        //     nums[i]=arr[j--];
+        // }
     }
 };
